14a8d270c933fb72e08852599953059dd9ca246d
jeromydean <jeromydean@gmail.com>
2026-02-21 11:44:49 -0500

    Add LiveCharts2 library
    Downgrade Avalonia libraries to be compatible with LiveCharts

--- src/BlueFence.DatabaseSentinel/ViewModels/MainWindowViewModel.cs
+++ src/BlueFence.DatabaseSentinel/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,24 @@
-﻿namespace BlueFence.DatabaseSentinel.ViewModels
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+
+namespace BlueFence.DatabaseSentinel.ViewModels
 {
   public partial class MainWindowViewModel : ViewModelBase
   {
     public string Greeting { get; } = "Welcome to Avalonia!";
+
+    public ISeries[] DashboardSeries { get; } =
+    [
+      new LineSeries<double>
+      {
+        Name = "Sample metric",
+        Values = new double[] { 12, 15, 18, 14, 20, 22, 19 }
+      },
+      new ColumnSeries<double>
+      {
+        Name = "Sample count",
+        Values = new double[] { 8, 11, 9, 14, 12, 10, 13 }
+      }
+    ];
   }
 }
